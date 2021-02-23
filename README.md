09648c3b681ccf2d6f11e208f5a6785b7ff0e478
Stuart Larsen <c0nrad@c0nrad.io>
2021-02-23 15:55:56 -0500

    before Toffoli

--- README.md
+++ README.md
@@ -11,3 +11,22 @@ The goal of this project is to see if we can make the circuit for "A=A\*B" small
 Demo so far:
 
 ![alt](/mulbrute.gif)
+
+# Method
+
+Brute force. Basically the algorithm tries every possible combination of CNOT gates and checks to see if the results are correct.
+
+For example this gate was randomly created. A = [0,1], B=[2,3,4,5]
+
+```
+ 0: ──╭|0.0⟩──╭C──────╭C──╭X──┤ ⟨Z⟩
+ 1: ──├|0.0⟩──│───╭X──│───│───┤ ⟨Z⟩
+ 2: ──├|0.0⟩──│───│───│───│───┤ ⟨Z⟩
+ 3: ──├|0.0⟩──╰X──╰C──│───│───┤ ⟨Z⟩
+ 4: ──├|0.0⟩──────────│───╰C──┤ ⟨Z⟩
+ 5: ──╰|0.0⟩──────────╰X──────┤ ⟨Z⟩
+```
+
+Then I measure a couple of times to see if corresponds to the multiplication table. So I plug in A=01, and B = 10, and then check after the circuit executes if B = 0010.
+
+It uses pennylanes default qubit simulator to check the circuits.
--- mulbrute.py
+++ mulbrute.py
@@ -29,76 +29,82 @@ def i_to_basis(a, pad=0):
 
 
 def basis_to_i(b):
-    out = 0
-    for i in range(len(b)):
-        if b[i]:
-            out += i**2
-    return out
+    return int("".join([str(int(c)) for c in b]), 2)
 
 
 class Circuit():
-
     def __init__(self, aSize, bSize, gates):
         self.gates = gates
         self.aSize = aSize
         self.bSize = bSize
-
-    def test(self):
-        dev = qml.device(
+        self.dev = qml.device(
             "default.qubit", wires=self.aSize + self.bSize)
 
-        @qml.qnode(dev)
-        def circuit():
-            aBasis = i_to_basis(a, pad=self.aSize)
-            bBasis = i_to_basis(b, pad=self.aSize)
-            zeros = np.zeros(self.bSize - self.aSize)
-            print(aBasis, zeros)
-            qml.BasisState(np.concatenate((np.array(aBasis + bBasis), zeros)), wires=range(
-                self.aSize + self.bSize))
+    def circuit(self, a, b):
+        aBasis = i_to_basis(a, pad=self.aSize)
+        bBasis = i_to_basis(b, pad=self.aSize)
+        zeros = [0] * (self.bSize - self.aSize)
+        qml.BasisState(np.array(aBasis + zeros + bBasis), wires=range(
+            self.aSize + self.bSize))
 
-            for g in self.gates:
-                qml.CNOT(wires=[g[0], g[1]])
+        for g in self.gates:
+            qml.CNOT(wires=[g[0], g[1]])
 
-            # return qml.expval(qml.PauliZ(0))
-            return [qml.expval(qml.PauliZ(i)) for i in range(self.aSize + self.bSize)]
+        return [qml.expval(qml.PauliZ(i)) for i in range(self.aSize + self.bSize)]
 
-        solutions = SolutionTable(self.aSize)
-        allCorrect = True
-        for a in range((self.aSize ** 2) - 1):
-            for b in range((self.aSize**2) - 1):  # use aSize again
+    def print(self):
+        circuit = qml.QNode(self.circuit, self.dev)
+        circuit(1, 1)
+        print(circuit.draw())
 
-                print("Testing", a, "*", b)
+    def execute(self, circuit, a, b):
+        results = circuit(a, b)
+        for i in range(len(results)):
+            if results[i] == -1:
+                results[i] = 1
+            elif results[i] == 1:
+                results[i] = 0
+            else:
+                raise Exception("this shouldn't be possible")
 
-                results = circuit()
-                for i in range(len(results)):
-                    if results[i] == -1:
-                        results[i] = 1
-                    elif results[i] == 1:
-                        results[i] = 0
-                    else:
-                        raise Exception("this shouldn't be possible")
+        aOut = basis_to_i((results[0:self.aSize]))
+        bOut = basis_to_i((results[self.aSize:]))
 
-                print(results)
-                if a == 0 and b == 0:
-                    print(circuit.draw())
+        return (aOut, bOut)
 
-                aOut = basis_to_i(results[0:self.aSize])
-                bOut = basis_to_i(results[self.aSize:])
+    def test(self):
+        circuit = qml.QNode(self.circuit, self.dev)
 
-                if (a*b) == bOut and a = aOut:
-                    print("NICE")
-                else:
-                    allCorrect = False
-                    print("Not a valid circuit")
-                    return
-        print("Holy crap!")
-        os.exit(1)
+        correct = 0
+        for a in range((2 ** self.aSize)):
+            for b in range((2 ** self.aSize)):
+                aOut, bOut = self.execute(circuit, a, b)
+                if (a*b) == bOut and aOut == a:
+                    correct += 1
+        return correct
+
+    def results(self):
+        circuit = qml.QNode(self.circuit, self.dev)
+
+        results = []
+        for a in range((2 ** self.aSize)):
+            for b in range((2 ** self.aSize)):
+                aOut, bOut = self.execute(circuit, a, b)
+
+                results.append((a, b, aOut, bOut))
+
+        return results
 
 
-class BruteForcer():
+class CircuitGenerator():
     def __init__(self, sizeA, sizeB):
         self.sizeA = sizeA
         self.sizeB = sizeB
+
+        self.generator = self.generateCircuits()
+
+    def next(self):
+        return next(self.generator)
 
     def generateCircuits(self):
         twoWirePerms = list(permutations(range(self.sizeA + self.sizeB), 2))
@@ -115,19 +121,54 @@ class BruteForcer():
         return
 
 
+class BruteForcer():
+    def __init__(self, N):
+        self.shots = 0
+        self.N = N
+        self.generator = CircuitGenerator(N, N+N)
+
+        self.bestGateSet = 0
+        self.bestGateSetScore = 0
+
+        self.maxScore = 2 * 2**N
+
+    def run(self):
+        gateSet = self.generator.next()
+        c = Circuit(self.N, self.N*2, gateSet)
+        score = c.test()
+
+        if score > self.bestGateSetScore:
+            print("New Best", score, self.maxScore)
+            self.bestGateSet = gateSet
+            self.bestGateSetScore = score
+            c.print()
+            print(c.results())
+
+            if score == self.maxScore:
+                print("ALL DONE!")
+
+        self.shots += 1
+        if self.shots % 100 == 0:
+            print(self.shots)
+            c.print()
+
+
 if __name__ == "__main__":
     assert i_to_binary(3) == "11"
     assert i_to_binary(3, pad=5) == "00011"
 
     assert i_to_basis(3) == [1, 1]
+    print(basis_to_i([1, 1]))
+    assert basis_to_i([1, 1]) == 3
+    assert basis_to_i([1, 0]) == 2
+    assert basis_to_i([0]) == 0
 
     solutions = SolutionTable(2)
     assert solutions.get(1, 2) == "10"
     assert solutions.get(3, 1, pad=5) == "00011"
 
-    b = BruteForcer(2, 4)
+    N = 1
 
-    for gateSet in b.generateCircuits():
-        print(gateSet)
-        c = Circuit(2, 4, gateSet)
-        c.test()
+    b = BruteForcer(N)
+    while True:
+        b.run()
